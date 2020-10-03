6686edb04401e43c5784954a2f69d94ccbb1ac0b
Aaron Urkin <a.urkin@outlook.com>
2020-10-03 10:03:19 +0300

    Authenticate endpoint has been implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { UsersService } from 'src/users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [
-    JwtStrategy,
-    UsersService
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '30d'),
+        },
+      })
+    })
   ],
-  controllers: []
+  providers: [
+    AuthService,
+    JwtStrategy,
+    UsersService,
+    LocalStrategy
+  ],
+  controllers: [AuthController]
 })
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly userService: UsersService
+    ) {}
+
+    async createToken(user: User) {
+        return {
+            access_token: this.jwtService.sign({
+                sub: user.id,
+                userType: user.type,
+                userName: user.userName
+            }),
+        }
+    }
+
+    async validateUser(email: string, password: string) {
+
+        const user = await this.userService.findByUserName(email);
+
+        //TODO: Add password encription and use password hash comparison instead
+        if ( user !== undefined && user.password == password ) {
+            return user;
+        }
+
+        return null;
+    }
+}

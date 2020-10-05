bd8c4fe4006dc2d72f92ad3821f5934d1796d508
Aaron Urkin <a.urkin@outlook.com>
2020-10-05 11:54:36 +0300

    Mock data services initialization was added

--- src/deliveries/deliveries.service.ts
+++ src/deliveries/deliveries.service.ts
@@ -9,6 +9,47 @@ import { GetPagedDeliveriesRequest } from '../requests/get-paged-deliveries.requ
 export class DeliveriesService {
 
     constructor(private readonly db: InMemoryDBService<DeliveryEntity>) {
+
+        const deliveries: Array<Partial<DeliveryEntity>> = [
+            {
+                cost: 20.5,
+                from: "Tel Aviv",
+                packageSize: 12.5,
+                to: "Rishon LeZion",
+                name: "Test Delivery 1",
+                description: "My test delivery 1",
+                id: "792d0b94-af8f-489c-b4a9-16a32445f985",
+                senderId: "2ff85781-7adb-484f-a70b-86adacd22242"
+            },
+            {
+                cost: 27.8,
+                from: "Rishon LeZion",
+                packageSize: 22.5,
+                to: "Tel Aviv",
+                name: "Test Delivery 2",
+                description: "My test delivery 2",
+                id: "792d0b94-af8f-489c-b4a9-16a32445f986",
+                senderId: "2ff85781-7adb-484f-a70b-86adacd22242"
+            },
+            {
+                cost: 120.7,
+                from: "Tel Aviv",
+                packageSize: 1.9,
+                to: "Haifa",
+                name: "Test Delivery 3",
+                description: "My test delivery 3",
+                id: "792d0b94-af8f-489c-b4a9-16a32445f987",
+                senderId: "2ff85781-7adb-484f-a70b-86adacd22242"
+            }
+        ];
+
+        deliveries.forEach(delivery => this.create(delivery));
+
+        deliveries[0].assignedTo = "58384f11-62e0-4397-8c94-16a43bde8fd8";
+        deliveries[2].assignedTo = "58384f11-62e0-4397-8c94-16a43bde8fd8";
+
+        this.assign(deliveries[0]);
+        this.assign(deliveries[2]);
     }
 
     async create(model: Partial<DeliveryEntity>): Promise<DeliveryEntity> {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,27 +5,24 @@ import { UserEntity } from '../entities/user.entity';
 @Injectable()
 export class UsersService {
 
-    // private users: Array<User>;
-
     constructor(private readonly db: InMemoryDBService<UserEntity>) {
-        /*
-        this.users = [
+
+        const users: Array<Partial<UserEntity>> = [
             {
-                id: 1,
                 type: 'sender',
                 userName: 'sender1',
                 password: 'sender1Password',
-                createdAt: new Date()
+                id: "2ff85781-7adb-484f-a70b-86adacd22242"
             },
             {
-                id: 2,
                 type: 'courier',
                 userName: 'courier1',
                 password: 'courier1Password',
-                createdAt: new Date()
+                id: "58384f11-62e0-4397-8c94-16a43bde8fd8"
             }
         ];
-        */
+
+        users.forEach(user => this.create(user));
     }
 
     async create(user: Partial<UserEntity>): Promise<UserEntity> {
